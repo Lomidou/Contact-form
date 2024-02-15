b37d6961abd5e7a924f8a526c0ff1b96001ab17c
Julien-Baiwir <julien.freddy.baiwir@gmail.com>
2024-02-15 16:14:31 +0100

    password julien

--- Php/db.inc.php
+++ Php/db.inc.php
@@ -3,7 +3,7 @@
 function connect_db() {
     $dsn = 'mysql:dbname=hackers_poulette;host=localhost';
     $user = 'root';
-    $password = 'root';
+    $password = 'rA4$tk7#LoPz&';
     $pdo = new PDO($dsn, $user, $password);
 
     return $pdo;
